539590d53eabc928036a263ecbb91aa2534374a0
MihaelaVacarus <mihaela.vacarus@outlook.com>
2021-05-22 16:18:17 +0000

    Remove datepicker from add game template

--- static/js/script.js
+++ static/js/script.js
@@ -1,14 +1,6 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
     $('.parallax').parallax();
-    $('.datepicker').datepicker({
-        format: "yyyy",
-        yearRange: 5,
-        showClearBtn: true,
-        i18n: {
-            done: "Select"
-        }
-    });
     $('select').formSelect();
     $('.tooltipped').tooltip();
     $('.modal').modal();
